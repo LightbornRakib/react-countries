1587c196e7688d5cf7854332aff1f9928f7f85c0
Naimul Islam <naimulislam@Naimuls-MacBook-Air.local>
2025-09-25 09:23:24 +0600

    3

--- src/assets/Country/Country.jsx
+++ src/assets/Country/Country.jsx
@@ -14,7 +14,7 @@ const Country = ({country}) => {
             <h1>Population: {country.population.population} <br />
                 {country.population.population > 300000? 'Populated': 'Not Populated'}
             </h1>
-            <button className='bg-blue-500 text-white p-2 rounded-md hover:bg-green-600 grid justify-self-bottom' onClick={handleAddVisited}>{visited? 'Remove Visited': 'Add Visited'}</button><br />
+            <button className='bg-blue-500 text-white p-2 rounded-md hover:bg-green-600 grid ' onClick={handleAddVisited}>{visited? 'Visited': 'Not Visited'}</button><br />
         </div>
     );
 };
